8914ab8ed2837551eb4ce28d587a675d34b8d337
poeMoon0416 <siz0416z@gmail.com>
2024-10-11 03:10:02 +0900

    [fix]Deno KV settings.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,15 +4,17 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $index from "./routes/index.tsx";
 import * as $recipes_id_ from "./routes/recipes/[id].ts";
 import * as $recipes_index from "./routes/recipes/index.ts";
 
-import type { Manifest } from "$fresh/server.ts";
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/index.tsx": $index,
     "./routes/recipes/[id].ts": $recipes_id_,
     "./routes/recipes/index.ts": $recipes_index,
   },
